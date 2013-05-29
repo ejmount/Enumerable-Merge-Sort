11c25ff1842903b56be9baffd019d474ce8c5643
unknown <gentlemanoctopus@github.fake>
2013-05-29 16:58:58 +0100

    Tidied, removed redundant fields; Enumerator is now public so it can be fed any seekable stream.

--- MergeSort/LineEnumerator.cs
+++ MergeSort/LineEnumerator.cs
@@ -2,29 +2,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace MergeSort
 {
-	class LineEnumerator : IEnumerator<string>, IEnumerator
+	/// <summary>
+	/// Wrapper to allow files to be treated as IEnumerables, e.g. for merge sorting.
+	/// 
+	/// </summary>
+	public class FileLineEnumerable : IEnumerable<string>
 	{
-		private StreamReader reader;
+		private FileInfo file;
+
+		public FileLineEnumerable(FileInfo F)
+		{
+			this.file = F;
+		}
+
+		IEnumerator<string> IEnumerable<string>.GetEnumerator()
+		{
+			return new LineEnumerator(File.OpenRead(file.FullName));
+			#warning File handles might be leaking. 
+			// The project I originally wrote this for was too quick/dirty/small to care about leaked filehandles,
+			// but a bigger project might need to watch for them. 
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable<string>)this).GetEnumerator();
+		}
+	}
+
+	
+	public class LineEnumerator : IEnumerator<string>, IEnumerator
+	{
 		protected const int pageSize = 4 * 1024;
 		protected char[] blockBuffer = new char[2 * pageSize];
 		protected char[] currentLine = new char[1024];
 		protected int currentLineLength = 0;
-		protected int lengthOfBlockContents = -1;
 		protected int cursor = 0;
-		protected int uncopiedStringLength = 0;
-
+		protected int lengthOfBlockContents = -1;
 		protected int Line = 0;
-
-
-		public LineEnumerator(Stream s)
-		{
-			if (!s.CanSeek) throw new ArgumentException();
-			reader = new StreamReader(s);
-		}
+		protected StreamReader reader;
+		protected int uncopiedStringLength = 0;
 
 		public string Current
 		{
@@ -32,33 +51,21 @@ namespace MergeSort
 			protected set;
 		}
 
-		public void Dispose()
-		{
-			reader.Close();
-		}
-
 		object System.Collections.IEnumerator.Current
 		{
 			get { return this.Current; }
 		}
 
-		private void SaveStringSoFar()
+		public LineEnumerator(Stream s)
 		{
-			Array.Copy(blockBuffer, cursor, currentLine, currentLineLength, uncopiedStringLength);
-			currentLineLength += uncopiedStringLength;
-			
+			if (!s.CanSeek) throw new ArgumentException();
+			reader = new StreamReader(s);
 		}
 
-		private void ExpandCurrentLineBuffer()
+		public void Dispose()
 		{
-			var temp = new char[currentLine.Length * 2];
-			for (int i = 0; i < currentLine.Length; i++)
-			{
-				temp[i] = currentLine[i];
-			}
-			currentLine = temp;
+			reader.Close();
 		}
-
 
 		public bool MoveNext()
 		{
@@ -85,12 +92,6 @@ namespace MergeSort
 						ExpandCurrentLineBuffer();
 					}
 
-
-
-
-
-
-
 					if ((cursor + uncopiedStringLength >= lengthOfBlockContents
 						|| NewlineAtIndex(cursor + uncopiedStringLength)))
 					{
@@ -101,7 +102,7 @@ namespace MergeSort
 						Line++;
 						currentLineLength = 0;
 						cursor += uncopiedStringLength;
-						cursor += System.Environment.NewLine.Length;
+						cursor += Environment.NewLine.Length;
 						uncopiedStringLength = 0;
 						break;
 					}
@@ -124,36 +125,9 @@ namespace MergeSort
 			}
 		}
 
-		private void CyclePageForward()
-		{
-			Array.Copy(blockBuffer, pageSize, blockBuffer, 0, pageSize);
-			lengthOfBlockContents -= pageSize;
-			lengthOfBlockContents += reader.ReadBlock(blockBuffer, lengthOfBlockContents, pageSize);
-			cursor -= pageSize;
-		}
-
-		static readonly char[] newline = System.Environment.NewLine.ToCharArray();
-		static readonly int newLineSize = System.Environment.NewLine.Length;
-		static readonly int negNewLineSize = -newLineSize;
-
-		private bool NewlineAtIndex(int pos)
-		{
-			if (pos <= negNewLineSize || pos >= lengthOfBlockContents) return false;
-
-			for (int i = 0; i < newline.Length; i++)
-			{
-				if (blockBuffer[pos + i] != newline[i] || pos + i >= lengthOfBlockContents)
-					return false;
-			}
-			return true;
-		}
-
-
-
 		public void Reset()
 		{
-			if (reader.BaseStream.CanSeek)
-			{
+			try {
 				reader.BaseStream.Seek(0, SeekOrigin.Begin);
 				blockBuffer = new char[2 * pageSize];
 				currentLine = new char[1024];
@@ -162,30 +136,46 @@ namespace MergeSort
 				cursor = 0;
 				uncopiedStringLength = 0;
 			}
-			else
+			catch (IOException e)
 			{
-				throw new NotSupportedException(); // We can't support the reset
+				throw new NotSupportedException("Cannot reset Enumerator", e);
 			}
 		}
-	}
-	
-	public class FileLineEnumerable : IEnumerable<string>
-	{
-		FileInfo file;
 
-		public FileLineEnumerable(FileInfo F)
+		private void CyclePageForward()
 		{
-			this.file = F;
+			Array.Copy(blockBuffer, pageSize, blockBuffer, 0, pageSize);
+			lengthOfBlockContents -= pageSize;
+			lengthOfBlockContents += reader.ReadBlock(blockBuffer, lengthOfBlockContents, pageSize);
+			cursor -= pageSize;
 		}
 
-		IEnumerator<string> IEnumerable<string>.GetEnumerator()
+		private void ExpandCurrentLineBuffer()
 		{
-			return new LineEnumerator(File.OpenRead(file.FullName));
+			var temp = new char[currentLine.Length * 2];
+			for (int i = 0; i < currentLine.Length; i++)
+			{
+				temp[i] = currentLine[i];
+			}
+			currentLine = temp;
 		}
 
-		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		private bool NewlineAtIndex(int pos)
 		{
-			return new LineEnumerator(File.OpenRead(file.FullName));
+			if (pos <= -Environment.NewLine.Length || pos >= lengthOfBlockContents) return false;
+
+			for (int i = 0; i < Environment.NewLine.Length; i++)
+			{
+				if (blockBuffer[pos + i] != Environment.NewLine[i] || pos + i >= lengthOfBlockContents)
+					return false;
+			}
+			return true;
+		}
+
+		private void SaveStringSoFar()
+		{
+			Array.Copy(blockBuffer, cursor, currentLine, currentLineLength, uncopiedStringLength);
+			currentLineLength += uncopiedStringLength;
 		}
 	}
 }
